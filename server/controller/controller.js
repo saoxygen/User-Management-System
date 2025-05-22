efdb1b83a0a973150e1d4e738d95fe6215aa2d1a
Kopano <Kopano@securitybis.co.za>
2025-05-22 17:18:51 +0200

    Create api completed, user info gets stored into mongoDB

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -5,8 +5,26 @@ exports.create = (req, res) => {
     // validate request
 
     if(!req.body){
-        res.status(400).send({message: })
+        res.status(400).send({message: "error"});
+        return
     }
+
+    // new user 
+    // note this matches the schema we set up in the model js file
+    const user = new userDB({
+        name: req.body.name,
+        email: req.body.email,
+        gender: req.body.gender,
+        status: req.body.status
+    });
+
+    // save user in the db
+    user.save(user)
+    .then(data => {
+        res.send(data)
+    }).catch(e => {
+        res.status(500).send({message: e.message || "Oops an error happened while creating the new user"})
+    });
 }
 
 // retrieve and return all users/ single user
